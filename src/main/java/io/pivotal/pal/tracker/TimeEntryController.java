42737bae68702a52c9aae185c66bd83ada338ed7
newtext <alancao101@gmail.com>
2019-12-04 09:56:57 -0500

    .

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,6 +1,4 @@
 package io.pivotal.pal.tracker;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 public class TimeEntryController {
 
     TimeEntryRepository timeEntryRepository;
-    Logger logger = LoggerFactory.getLogger(TimeEntryController.class);
 
     public TimeEntryController(TimeEntryRepository timeEntryRepository) {
         this.timeEntryRepository = timeEntryRepository;
